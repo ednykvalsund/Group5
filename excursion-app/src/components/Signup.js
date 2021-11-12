147e126605bf8c1fd4334828a886a7248426c83d
vdfrede <vdfrede@gmail.com>
2021-11-12 13:19:15 +0100

    minor things to signup page

--- excursion-app/src/components/Signup.js
+++ excursion-app/src/components/Signup.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Card from "./Card";
 
 function Signup() {
   return (
     <div className="Signup-page">
       <h1>Signup component</h1>
+      <div className="flex-container">
+        <Card id="0" headline="Add person">
+          <p>Hellotest</p>
+        </Card>
+        <Card id="0" headline="Add drive">
+          <p>Component </p>
+          <p>is a just a child</p>
+        </Card>
+        <Card id="0" headline="Registered"></Card>
+      </div>
     </div>
   );
 }
--- excursion-app/src/components/TESTPAGE.js
+++ excursion-app/src/components/TESTPAGE.js
@@ -6,15 +6,7 @@ function TestPage() {
     <div className="TestPage-component">
       <h1>Test Page</h1>
       <div className="flex-container">
-          <Card id="0" headline="Assign duties">
-            <p>Hellotest</p>
-          </Card>
-          <Card id="0">
-            <p>Component with</p>
-            <p>no headline. This</p>
-            <p>is a just a child</p>
-          </Card>
-          <Card id="0" headline="Assign duties"></Card>
+        
         </div>
 
       
