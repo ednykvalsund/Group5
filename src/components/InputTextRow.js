c6f37caec7652ed347ddf2692b330883c514bb6d
vdfrede <vdfrede@gmail.com>
2021-11-15 11:16:40 +0100

    changed label on textfield, we should agree on labels

--- src/components/InputTextRow.js
+++ src/components/InputTextRow.js
@@ -1,26 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import * as React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -28,8 +5,17 @@ import TextField from "@mui/material/TextField";
 export default function SimpleTextField(props) {
   return (
     <div className="Forms">
-      <TextField fullWidth hiddenLabel id="outlined-basic"  variant="outlined" placeholder={props.title}
-          />
+      <TextField
+        fullWidth
+       
+        variant="outlined"
+        label={props.title}
+        InputLabelProps={{
+          shrink: true,
+          disableAnimation: true
+        }}
+       // placeholder={props.title}
+      />
     </div>
   );
 }
--- src/components/pages/TESTPAGE.js
+++ src/components/pages/TESTPAGE.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Card from "../Card";
 import Steppers from "../Progress2";
+import BasicSelect from "../InputDropRow";
+import SimpleTextField from "../InputTextRow";
+import MultiSelect from "../InputDropMultiRow";
 function TestPage() {
   return (
     <div className="TestPage-component">
       <h1>Test Page</h1>
-      <div className="flex-container">
-        
-        </div>
-<Steppers steps={["Create Excursion", "Create duties", "Create shopping list"]} doneSteps={1}/>
-
+      <div className="flex-container"></div>
+      <Steppers
+        steps={["Create Excursion", "Create duties", "Create shopping list"]}
+        doneSteps={1}
+      />
+      <SimpleTextField title="Destination" />
+      <BasicSelect title="Year" options={["2021", "2022", "2023", "2024"]} />
+      <MultiSelect title="Year" options={["2021", "2022", "2023", "2024"]} />
     </div>
   );
 }
