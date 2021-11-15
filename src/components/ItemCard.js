133a24f1c72a7f4ab5db3c2f764eee203fd27c67
ednykvalsund <52636934+ednykvalsund@users.noreply.github.com>
2021-11-15 14:44:48 +0100

    improved styling for ItemCard, not done.

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,13 +3,9 @@ import React from "react"
 function ItemCard(props){
     return(
         <div className="item-card-container">
-            {props.textInpunt1}
-            {props.textInpunt2}
-            {props.textInpunt3}
-            {props.editBtn}
-            {props.deleteBtn}
-            
-            This is an item card
+            <div >
+                <p> {props.item} {props.amount} {props.unit} </p>
+            </div>
         </div>
     )
 }
