e1257fc1504b74c0bcbce50506a7394bd9330046
vdfrede <vdfrede@gmail.com>
2021-11-15 15:04:36 +0100

    spaced out itemcard text

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -3,8 +3,10 @@ import React from "react"
 function ItemCard(props){
     return(
         <div className="item-card-container">
-            <div >
-                <p> {props.item} {props.amount} {props.unit} </p>
+            <div className="item-card-text">
+                <span className="item-card-text"> {props.item}  </span>
+                <span className="item-card-text"> {props.amount}  </span>
+                <span className="item-card-text"> {props.unit} </span>
             </div>
         </div>
     )
