3ca338488ee41faddf199ec205d3e8b18d319ec1
vdfrede <vdfrede@gmail.com>
2021-11-22 12:24:06 +0100

    made done page

--- src/components/pages/DonePage.js
+++ src/components/pages/DonePage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TextButton from "../TextButton";
 
 function DonePage() {
   return (
     <div className="page-container">
       <h1 className="page-title">You're done!</h1>
+      <TextButton label="Return to main page" className="green-button" link="/"/>
     </div>
   );
 }
