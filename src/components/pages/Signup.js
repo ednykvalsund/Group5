73d5a4d76ad72bb88679bb5de15f73b25f65b04d
vdfrede <vdfrede@gmail.com>
2021-11-22 15:16:21 +0100

    functionality and state for signup plus front end

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -5,23 +5,76 @@ import Card from "../Card";
 import RadioButtons from "../RadioButtons";
 import UserCard from "../UserCard";
 import TextButton from "../TextButton";
+import { elemIndex } from "prelude-ls";
 
 function Signup(props) {
+  const [participant, setParticipant] = useState("Member");
+  const [drive, setDrive] = useState("Register car");
+  function memberOrExtra() {
+    if (participant === "Member") {
+      return (
+        <>
+          <SimpleTextField title="Email" />
+          <SimpleTextField title="Phone" />
+          <SimpleTextField title="Work phone" />
+          <SimpleTextField title="Address" />
+        </>
+      );
+    } else {
+      return <></>;
+    }
+  }
 
-  const [participant, setParticipant] = useState('Member')
+  function carOrSeat() {
+    if (drive === "Register car") {
+      return (
+        <>
+          <SimpleTextField title="Registration number" />
+          <div className="inline-forms">
+            <BasicSelect
+              title="Color"
+              options={[
+                "White",
+                "Black",
+                "Grey",
+                "Red",
+                "Yellow",
+                "Orange",
+                "Blue",
+                "Green",
+                "Purple",
+              ]}
+            />
+            <SimpleTextField title="Free seats" />
+          </div>
+          <SimpleTextField title="Leaves from" />
+        </>
+      );
+    } else {
+      return(
+      <BasicSelect
+        title="Leaves from"
+        options={["Address 1", "Address 2", "Address 3"]}
+      />);
+    }
+  }
+
+  console.log(participant);
   return (
     <div className="page-container">
       <h1 className="page-title">{props.title}</h1>
       <div className="card-container">
         <div className="flex-container">
           <Card id="0" headline="Add person">
-            <RadioButtons value={participant} onChange={(e) => setParticipant(e.target.value)}label1="Member" label2="Extra" />
+            <RadioButtons
+              value={participant}
+              onChange={(e) => setParticipant(e.target.value)}
+              label1="Member"
+              label2="Extra"
+            />
             <div className="card-textfields-container">
               <SimpleTextField title="Name" />
-              <SimpleTextField title="Email" />
-              <SimpleTextField title="Phone" />
-              <SimpleTextField title="Work phone" />
-              <SimpleTextField title="Address" />
+              {memberOrExtra()}
               <BasicSelect
                 title="Age group"
                 options={["Adult", "Teenager", "Child"]}
@@ -30,27 +83,14 @@ function Signup(props) {
             <TextButton label="Save" className="green-button" />
           </Card>
           <Card id="0" headline="Add drive">
-            <RadioButtons label1="Register car" label2="Reserve seat" />
+            <RadioButtons
+              value={drive}
+              onChange={(e) => setDrive(e.target.value)}
+              label1="Register car"
+              label2="Reserve seat"
+            />
             <div className="card-textfields-container">
-              <SimpleTextField title="Registration number" />
-              <div className="inline-forms">
-                <BasicSelect
-                  title="Color"
-                  options={[
-                    "White",
-                    "Black",
-                    "Grey",
-                    "Red",
-                    "Yellow",
-                    "Orange",
-                    "Blue",
-                    "Green",
-                    "Purple",
-                  ]}
-                />
-                <SimpleTextField title="Free seats" />
-              </div>
-              <SimpleTextField title="Leaves from" />
+            {carOrSeat()}
             </div>
             <TextButton label="Add" className="green-button" />
           </Card>
