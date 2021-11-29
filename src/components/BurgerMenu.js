9f5cc0e56c8835092b49942c77ca42068030c905
Jepmoltho <jept@itu.dk>
2021-11-29 17:55:54 +0100

    Simplified the funtion calls for the burger menu

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -4,66 +4,51 @@ import { useNavigate } from "react-router-dom";
 
 //https://www.npmjs.com/package/react-burger-menu
 
-function BurgerMenu (){
-  // showSettings(event) {
-  //   event.preventDefault();
-  // }
- 
-    const navigate = useNavigate();
-    
-    function NavHome() {
-      navigate("/");
-    };
+function BurgerMenu() {
+  const navigate = useNavigate();
 
-    function NavOrganisator() {
-      navigate("/organisator");
-    };
-
-    function NavCreateDuties() {
-      navigate("/add-duties");
-    };
-
-    function NavAssignDuties() {
-      navigate("assign-duties");
-    };
-
-    function NavShoppingList() {
-      navigate("/shopping-list");
-    };
-
-    function NavParticipant() {
-      navigate("/participant");
-    };
-  
-    function NavTest() {
-      navigate("/Test");
-    };
-  
-    return (
-      <Menu>
-        <a id="home" className="menu-item" onClick={NavHome}>
-          Main page
-        </a>
-        <a id="organisator" className="menu-item" onClick={NavOrganisator}>
-          Organisator
-        </a>
-        <a id="create-duties" className="menu-item" onClick={NavCreateDuties}>
-          Add duties
-        </a>
-        <a id="assign-duties" className="menu-item" onClick={NavAssignDuties}>
-          Assign duties
-        </a>
-        <a id="shopping-list" className="menu-item" onClick={NavShoppingList}>
-          Shopping list
-        </a>
-        <a id="participant" className="menu-item" onClick={NavParticipant}>
-          Participant
-        </a>
-        <a id="participant" className="menu-item" onClick={NavTest}>
-          Test Page
-        </a>
-      </Menu>
-    );
-  }
+  return (
+    <Menu>
+      <a id="home" className="menu-item" onClick={() => navigate("/")}>
+        Main page
+      </a>
+      <a
+        id="organisator"
+        className="menu-item"
+        onClick={() => navigate("/organisator")}
+      >
+        Organisator
+      </a>
+      <a
+        id="create-duties"
+        className="menu-item"
+        onClick={() => navigate("/add-duties")}
+      >
+        Add duties
+      </a>
+      <a
+        id="assign-duties"
+        className="menu-item"
+        onClick={() => navigate("assign-duties")}
+      >
+        Assign duties
+      </a>
+      <a
+        id="shopping-list"
+        className="menu-item"
+        onClick={() => navigate("/shopping-list")}
+      >
+        Shopping list
+      </a>
+      <a
+        id="participant"
+        className="menu-item"
+        onClick={() => navigate("/sign-up")}
+      >
+        Participant
+      </a>
+    </Menu>
+  );
+}
 
 export default BurgerMenu;
