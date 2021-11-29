e188260571b5f67467948ec5089eb7f0ee4d5356
Jonas Krohn <jkak@itu.dk>
2021-11-29 17:34:39 +0100

    Burgermenu uses navigate() now. But code can be very much neater

--- src/components/BurgerMenu.js
+++ src/components/BurgerMenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { slide as Menu } from "react-burger-menu";
+import { useNavigate } from "react-router-dom";
 
 //https://www.npmjs.com/package/react-burger-menu
 
@@ -7,28 +8,58 @@ function BurgerMenu (){
   // showSettings(event) {
   //   event.preventDefault();
   // }
+ 
+    const navigate = useNavigate();
+    
+    function NavHome() {
+      navigate("/");
+    };
 
+    function NavOrganisator() {
+      navigate("/organisator");
+    };
+
+    function NavCreateDuties() {
+      navigate("/add-duties");
+    };
+
+    function NavAssignDuties() {
+      navigate("assign-duties");
+    };
+
+    function NavShoppingList() {
+      navigate("/shopping-list");
+    };
+
+    function NavParticipant() {
+      navigate("/participant");
+    };
+  
+    function NavTest() {
+      navigate("/Test");
+    };
+  
     return (
       <Menu>
-        <a id="home" className="menu-item" href="/">
+        <a id="home" className="menu-item" onClick={NavHome}>
           Main page
         </a>
-        <a id="organisator" className="menu-item" href="/organisator">
+        <a id="organisator" className="menu-item" onClick={NavOrganisator}>
           Organisator
         </a>
-        <a id="create-duties" className="menu-item" href="/add-duties">
+        <a id="create-duties" className="menu-item" onClick={NavCreateDuties}>
           Add duties
         </a>
-        <a id="assign-duties" className="menu-item" href="/assign-duties">
+        <a id="assign-duties" className="menu-item" onClick={NavAssignDuties}>
           Assign duties
         </a>
-        <a id="shopping-list" className="menu-item" href="/shopping-list">
+        <a id="shopping-list" className="menu-item" onClick={NavShoppingList}>
           Shopping list
         </a>
-        <a id="participant" className="menu-item" href="/sign-up">
+        <a id="participant" className="menu-item" onClick={NavParticipant}>
           Participant
         </a>
-        <a id="participant" className="menu-item" href="/test">
+        <a id="participant" className="menu-item" onClick={NavTest}>
           Test Page
         </a>
       </Menu>
