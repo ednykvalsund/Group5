d45e8a0ccc28eba84cb45fd16d2d8a03ce1ad096
Jonas Krohn <jkak@itu.dk>
2021-12-03 15:47:34 +0100

    Messy implementation of assignduties functionality

--- src/components/pages/AssignDuties.js
+++ src/components/pages/AssignDuties.js
@@ -3,10 +3,84 @@ import Card from "../Card";
 import ItemCard from "../ItemCard";
 import MultiSelect from "../InputDropMultiRow";
 import TextButton from "../TextButton";
+import { useState, useContext, useEffect } from "react";
+import ExcursionContext from "../../ExcursionContext";
+import Parse from "parse";
+import BasicSelect from "../InputDropRow";
 
 function AssignDuties(props) {
-  const duties = ["Duty 1", "Duty 2"];
   const options = ["test 1", "test 2"];
+  const { excursionContext } = useContext(ExcursionContext);
+  const [DutyList, setDutyList] = useState([]);
+  const [ParticipantList, setParticipantList] = useState([]);
+
+  useEffect(() => {
+    readDuties();
+    readParticipants();
+    // console.log("An excursion context:", excursionContext);
+    //Renders duties connected with current context upon load. Corresponds to the lifecycle-method: componentDidMount(). The second param [] ensures it only runs once upon load, otherwise it keeps running and we will get a parse-error from back4app
+  }, []);
+
+  const readDuties = async function () {
+    // Reading parse objects is done by using Parse.Query
+    if (excursionContext) {
+      try {
+        const parseQuery = new Parse.Query("Duty");
+        parseQuery.contains("excursionId", excursionContext);
+        // console.log("This", excursionContext);
+
+        let duties = await parseQuery.find();
+
+        // Be aware that empty or invalid queries return as an empty array
+        // Set results to state variable
+        setDutyList(duties);
+        console.log(duties);
+        return true;
+      } catch (error) {
+        // Error can be caused by lack of Internet connection
+        alert(error);
+        return false;
+      }
+    } else {
+    }
+  };
+
+  const [names, setNames] = useState([]);
+
+  const readParticipants = async function () {
+    // Reading parse objects is done by using Parse.Query
+    if (excursionContext) {
+      try {
+        const parseQuery = new Parse.Query("Duty");
+        parseQuery.contains("excursionId", excursionContext);
+        // console.log("This", excursionContext);
+
+        let duties = await parseQuery.find();
+
+        // Be aware that empty or invalid queries return as an empty array
+        // Set results to state variable
+        setParticipantList(duties);
+        {
+          ParticipantList.map((name) =>
+            setNames((prevState) => [...prevState, name.get("title")])
+          );
+        }
+        console.log("2:", names);
+
+        return true;
+      } catch (error) {
+        // Error can be caused by lack of Internet connection
+        alert(error);
+        return false;
+      }
+    } else {
+    }
+  };
+
+  const [select, setSelect] = useState("");
+  const handleSelect = (e) => {
+    setSelect(e.target.value);
+  };
 
   return (
     <div className="page-container">
@@ -14,10 +88,20 @@ function AssignDuties(props) {
       <div className="card-container">
         <Card>
           <div className="card-textfields-container">
-            {duties.map((duty) => (
-              <ItemCard item={duty}>
-                <MultiSelect title="Responsible" options={options} />
-                <MultiSelect title="Assign" options={options} />
+            {DutyList.map((duty) => (
+              <ItemCard id={duty.get("objectId")} item={duty.get("title")}>
+                <BasicSelect
+                  title="Responsible"
+                  options={names}
+                  handleChange={handleSelect}
+                  value={select}
+                />
+                <MultiSelect
+                  title="Assign"
+                  options={names}
+                  handleChange={handleSelect}
+                  value={select}
+                />
               </ItemCard>
             ))}
           </div>
