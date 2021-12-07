752e351a9becb52669ab09762d54585bb28276f3
vdfrede <vdfrede@gmail.com>
2021-12-07 12:08:36 +0100

    Merged jeppe and my changes in choose excursion

--- src/components/pages/ChooseExcursion.js
+++ src/components/pages/ChooseExcursion.js
@@ -3,18 +3,13 @@ import React, { useEffect, useState, useContext } from "react";
 import { getExcursions } from "../../data";
 import BasicSelect from "../InputDropRow";
 import TextButton from "../TextButton";
-import ExcursionContext from "../../ExcursionContext";
 import Parse from "parse";
 
 function ChooseExcursion() {
   const [excursions, setExcursions] = useState([]);
-  const { excursionContext, setExcursionContext } =
-    useContext(ExcursionContext);
 
   useEffect(() => {
     loadExcursions();
-    localStorage.setItem("currentExcursionId", select);
-    //Renders duties connected with current context upon load. Corresponds to the lifecycle-method: componentDidMount(). The second param [] ensures it only runs once upon load, otherwise it keeps running and we will get a parse-error from back4app
   }, []);
 
   async function loadExcursions() {
@@ -22,50 +17,16 @@ function ChooseExcursion() {
     setExcursions(options);
   }
 
-  async function SaveExcursion(e) {
-    //Set context here
-    setExcursionContext(e);
-    //localStorage.setItem("currentExcursionId", excursionContext);
-    //console.log(excursionContext);
-  }
   const [select, setSelect] = useState("");
   const handleSelect = (e) => {
     setSelect(e.target.value);
     setExcursionId(e.target.value);
-    //localStorage.setItem("currentExcursionId", e.target.value);
-    //console.log("target ", e.target.value);
-    //console.log("hook", select);
   };
 
-  async function setExcursionId(name) {
-    try {
-      const query = new Parse.Query("Excursion");
-      query.contains("destination", name);
-      const queryResult = await query.find();
-      const currentExcursion = queryResult[0];
-      const excursionId = currentExcursion.id;
-      localStorage.setItem("currentExcursionId", excursionId);
-    } catch (error) {
-      console.log("Error caught: ", error);
-    }
+  function setExcursionId(destination) {
+    let obj = excursions.find((o) => o.title === destination);
+    localStorage.setItem("currentExcursionId", obj.id);
   }
-
-  /*
-    const [memberId, setMemberId] = useState("");
-  async function fetchMemberId() {
-    try {
-      const query = new Parse.Query("Participant");
-      query.contains("firstName", firstName);
-      const queryResult = await query.find();
-      const currentPerson = queryResult[0];
-      const memId = currentPerson.id;
-      setMemberId(memId);
-      //setMemberId(queryResult.get("objectId"));
-    } catch (error) {
-      printError(error);
-    }
-  }*/
-
   return (
     <>
       <div className="page-container">
@@ -81,9 +42,8 @@ function ChooseExcursion() {
           <TextButton
             className="green-button"
             label="Next"
-            handleClick={SaveExcursion}
             link="/sign-up"
-            btnSwitch="HandleAndNav"
+            btnSwitch="Nav"
           ></TextButton>
         </div>
       </div>
