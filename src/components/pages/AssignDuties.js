49281d3d9d4dc510b2514dd38b39c91265d26a88
ednykvalsund <52636934+ednykvalsund@users.noreply.github.com>
2022-01-04 17:48:10 +0100

    Refactor: cleaned file for un used imports, console log, comments etc.

--- src/components/pages/AssignDuties.js
+++ src/components/pages/AssignDuties.js
@@ -9,40 +9,25 @@ import { getDuties, getParticipants } from "../../data";
 
 function AssignDuties(props) {
   const currentExcursionId = localStorage.getItem("currentExcursionId");
-  //console.log(currentExcursionId);
-  //const { excursionContext } = useContext(ExcursionContext);
   const [DutyList, setDutyList] = useState([]);
   const [ParticipantList, setParticipantList] = useState([]);
 
   useEffect(() => {
     getDuties(currentExcursionId, setDutyList);
     getParticipants(currentExcursionId, setParticipantList);
-    //Renders duties connected with current context upon load. Corresponds to the lifecycle-method: componentDidMount(). The second param [] ensures it only runs once upon load, otherwise it keeps running and we will get a parse-error from back4app
   }, [currentExcursionId]);
 
   const [selectResponsible, setSelectResponsible] = useState("");
   const handleSelectResponsible = (e) => {
     setSelectResponsible(e.target.value);
-    console.log("Goes in");
   };
 
   const [selectAssign, setSelectAssign] = useState("");
   const handleSelectAssign = (e) => {
     setSelectAssign(e.target.value);
-    console.log("Goes in");
   };
 
   const [assigned, setAssigned] = useState([]);
-
-  // function saveChange(array, dutyId, title, responsible, assigned) {
-  //   let assignedDuty = {
-  //     id: id,
-  //     duty: title,
-  //     res: responsible,
-  //     helpers: assigned,
-  //   };
-  //   array.push(assignedDuty);
-  // }
 
   return (
     <div className="page-container">
