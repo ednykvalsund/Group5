57dd39dda97ba546d1be0646599ffed6ca9cea63
vdfrede <vdfrede@gmail.com>
2022-01-06 21:33:59 +0100

    moved hashrouter back

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom";
 import "./App.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <HashRouter>
       <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
